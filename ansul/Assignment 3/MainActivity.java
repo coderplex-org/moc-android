84cb2109fb17165341e2dfef74918f06440cf1ca
Ansul Gupta <ansul.gupta2790@gmail.com>
2017-06-08 23:45:23 +0530

    Assignment 3 completed (#27)
    * Happy birthday card
    
    * Journal file
    
    * Added Facebook login using relative layout
    
    * Journal file changes
    
    * Journal changes
    
    * Assignment 3 part 1 completed

---
+++ ansul/Assignment 3/MainActivity.java
@@ -0,0 +1,65 @@
+package com.android.fcc.activitylifecycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        displayToast("Activity 1 onCreate");
+        setContentView(R.layout.activity_main);
+        Button button=(Button) findViewById(R.id.activity2_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                try{
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    displayToast("Going to activity 2");
+                    startActivity(intent);
+                }catch (Exception e){
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayToast("Activity 1 onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        displayToast("Activity 1 onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayToast("Activity 1 onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        displayToast("Activity 1 onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        displayToast("Activity 1 onDestroy");
+    }
+
+    private void displayToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ ansul/Assignment 3/SecondActivity.java
@@ -0,0 +1,62 @@
+package com.android.fcc.activitylifecycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by ansul on 07/06/17.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        displayToast("Activity 2 onCreate");
+        setContentView(R.layout.activity_second);
+        Button button=(Button) findViewById(R.id.activity1_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                displayToast("Going back to Activity 1");
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayToast("Activity 2 onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        displayToast("Activity 2 onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        displayToast("Activity 2 onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        displayToast("Activity 2 onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        displayToast("Activity 2 onDestroy");
+    }
+    private void displayToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
